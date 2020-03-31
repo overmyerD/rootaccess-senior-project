ccf9aed257fee726d8fb4d9befe30725c8f8b632
Christopher McNeil <christophermcneil9@outlook.com>
2020-03-31 18:16:01 -0500

    intialized new code

---
+++ christopher's code/reader.m
@@ -0,0 +1,21 @@
+inputdir = './senior_project/032720_18DAP/*.JPG';
+outputdir = './senior_project/Output_test/';
+
+% reads in all images in a given directory
+ds = imageDatastore(inputdir);
+
+% loops through every image in the directory
+for i = 1:size(ds.Files,1)
+    image = read(ds);
+    % crops the original image down to a new size
+    image = AutoCropper(image);
+    
+    % insert canny here
+    
+    % insert analysis here
+    
+    % writes the new image to a given directory
+    [filepath,name,ext] = fileparts(ds.Files{i});
+    new_filename = strcat(outputdir,name,"_cropped",ext);
+    imwrite(image, new_filename);
+end
