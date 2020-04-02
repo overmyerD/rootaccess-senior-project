d0ed10d44c9258c4b644282d7f3a0b8485c721dd
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-02 11:55:57 -0500

    deleted old code

--- Canny/canny.m
+++
@@ -1,7 +0,0 @@
-[filename,filepath] = uigetfile({'*.*;*.jpg;*.png;*.bmp;*.oct'}, 'Select File to Open');
-fullname = string([filepath, filename]);
-app.loadedImage = rgb2gray(imread(fullname));
-BW1 = edge(rgb2gray(imread(fullname)), 'Canny', [0.1, 0.5], 1);
-
-
-imshow(BW1);
