c0c089c5c4e3333bb4439b57e4f38f04bd7b3673
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-02 11:56:22 -0500

    intialized new code + updated old code

--- christopher's code/reader.m
+++ christopher's code/reader.m
@@ -6,16 +6,19 @@ ds = imageDatastore(inputdir);
 
 % loops through every image in the directory
 for i = 1:size(ds.Files,1)
-    image = read(ds);
-    % crops the original image down to a new size
-    image = AutoCropper(image);
-    
-    % insert canny here
-    
-    % insert analysis here
-    
-    % writes the new image to a given directory
-    [filepath,name,ext] = fileparts(ds.Files{i});
-    new_filename = strcat(outputdir,name,"_cropped",ext);
-    imwrite(image, new_filename);
+    if contains(ds.Files{i}, "BF") == 0
+        image = read(ds);
+        % crops the original image down to a new size
+        image = AutoCropper(image);
+        
+        % insert canny here
+        image = canny_algorithm(image);
+        
+        % insert analysis here
+        
+        % writes the new image to a given directory
+        [filepath,name,ext] = fileparts(ds.Files{i});
+        new_filename = strcat(outputdir,name,"_cropped",ext);
+        imwrite(image, new_filename);
+    end
 end
