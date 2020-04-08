4b6b7802f990699ea2219a637cb1a676d46f7a73
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-08 08:02:16 -0500

    added multi-folder functionality, removed imageDataStore()

--- christopher's code/reader.m
+++ christopher's code/reader.m
@@ -1,24 +1,27 @@
-inputdir = './senior_project/032720_18DAP/*.JPG';
-outputdir = './senior_project/Output_test/';
+rootdir = './senior_project/040120 testing';
+outputdir = './senior_project/Output_test2/';
+
+filelist = dir(fullfile(rootdir, '**/*.JPG'));  %get list of files and folders in any subfolder
+filelist = filelist(~[filelist.isdir]);  %remove folders from list
 
 % reads in all images in a given directory
-ds = imageDatastore(inputdir);
+% ds = imageDatastore(filelist.folder);
 
-% loops through every image in the directory
-for i = 1:size(ds.Files,1)
-    if contains(ds.Files{i}, "BF") == 0
-        image = read(ds);
+% loops through every image in the directory, skipping brightfield images
+for i = 1:size(filelist,1)
+    if contains(filelist(i).name, "BF") == 0 && contains(filelist(i).name, "white") == 0
+        image_name = strcat(filelist(i).folder,"/",filelist(i).name);
+        image = imread(image_name);
         % crops the original image down to a new size
         image = AutoCropper(image);
         
         % insert canny here
         image = canny_algorithm(image);
         
-        % insert analysis here
+        % insert analysis heres
         
         % writes the new image to a given directory
-        [filepath,name,ext] = fileparts(ds.Files{i});
-        new_filename = strcat(outputdir,name,"_cropped",ext);
+        new_filename = strcat(outputdir,filelist(i).name,"_cropped",".JPG");
         imwrite(image, new_filename);
     end
 end
