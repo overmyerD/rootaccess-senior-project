b338d4a3678ef1706d02f9bf6d7e7eafd39ad4b8
Dalton Overmyer <dalton@raptorpowersystems.com>
2020-04-08 17:13:34 -0500

    added reduce_pixel_density_matrix for testing

--- Analysis/pixel_density.m
+++ Analysis/pixel_density.m
@@ -1,25 +1,16 @@
-canny_directory = "./Images/Canny/21DAP";
+canny_directory = "./Images/Canny/26DAP";
 %canny_images = getFiles(canny_directory, "jpg");
 
-image = imread(strcat(canny_directory, "/0189.jpg"));
-
 %convert jpg to binary bitmap because it's not already
-image = imbinarize(rgb2gray(image));
-
-%get width and height of image
-width   = size(image, 2);
-height  = size(image, 1);
-
-%sum the columns into 1D arr
-pix_density = sum(image).';
-plot_pixel_density(pix_density, 'Root Density', 1);
+image_1 = imbinarize(rgb2gray(imread(strcat(canny_directory, "/0242.jpg"))));
+image_2 = imbinarize(rgb2gray(imread(strcat(canny_directory, "/0241.jpg"))));
 
 %reduction must be divisible by width
 reduction = 100;
 
-%reshape to 2D and sum again to 1D arr
-reduced_pix_density = reduce_pixel_density(pix_density, reduction);
+[reduced_pix_density_xyz, reduced_pix_density_xy, reduced_pix_density_zy, magnitude] = reduce_pixel_density_matrix(image_1, image_2, reduction);
 
-scale = idivide(length(pix_density), int16(reduction));
-plot_pixel_density(reduced_pix_density, 'Root Density', scale);
+bar3(reduced_pix_density_xyz);
+plot_pixel_density(reduced_pix_density_zy, 'zy pixel density', magnitude);
+plot_pixel_density(reduced_pix_density_xy, 'xy pixel density', magnitude);
 
