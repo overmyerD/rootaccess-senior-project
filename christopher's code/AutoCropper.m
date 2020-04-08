546bc58e0558fe2359747a2f974e0ef6325f95ab
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-08 08:02:44 -0500

    Started cropping height

--- christopher's code/AutoCropper.m
+++ christopher's code/AutoCropper.m
@@ -1,12 +1,21 @@
 function cropped_image = AutoCropper(image)
     % Predefined parameters
+    f = @(span,width) 2.^(-((1/width*4)*(span-(width/2))).^2);
+    g = @(span,height) 2.^(-((1/height*4)*(span-(height/8))).^2);
+    
     height = size(image,1);
     width = size(image,2);
     crop_width = 900;
-    crop_height = 930;
+    crop_height = 900;
+    
+    %%% Crops Width
+    % Creates a scalar function which is used to scale summed pixel value
+    span = linspace(1,width,width);
+    scalar = f(span,width);
 
     % Finds the summed pixel values for every column in green monochrome image
     values = sum((image(:,:,2)));
+    values = values.*scalar;
 
     % finds a number of local maxima across the distribution and what their
     % indicies are
@@ -27,6 +36,23 @@ function cropped_image = AutoCropper(image)
         centerpoint = indicies2(idx2);
     end
 
-    % crops the iamge to 900px width around the center point
-    cropped_image = imcrop(image,[centerpoint-crop_width/2,0,900,height]);
+    % crops the iamge to a preset width around the center point
+    cropped_image = imcrop(image,[centerpoint-crop_width/2,0,crop_width,height]);
+    
+    %%% Crops Height 
+    % creates a scalar function to augment
+    span = linspace(1,height,height);
+    scalar = g(span,height);
+    
+    % sums and scales the distribution of pixel's values in each column
+    values = sum(cropped_image(:,:,2)');
+    values = values.*scalar;
+    
+    % finds the maximum point in the 
+    m = max(values);
+    index = find(values == m);
+    
+    % crops the image to a pre set height based upon the max
+%     cropped_image = imcrop(cropped_image,[0,index+floor(height/48),width,crop_height]);
+    cropped_image = imcrop(cropped_image,[0,index-96,width,crop_height]);
 end
