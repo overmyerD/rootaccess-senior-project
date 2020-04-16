a7c711433e51d0194f064b4b938f98443585058b
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-16 15:33:20 -0500

    assorted changes

--- christopher's code/AutoCropper.m
+++ christopher's code/AutoCropper.m
@@ -1,12 +1,12 @@
 function cropped_image = AutoCropper(image)
     % Predefined parameters
-    f = @(span,width) 2.^(-((1/width*4)*(span-(width/2))).^2);
-    g = @(span,height) 2.^(-((1/height*4)*(span-(height/8))).^2);
+    f = @(span,width) 2.^(-((2/width*4)*(span-(width/2))).^2);
+    g = @(span,height) 2.^(-((1/height*4)*(span-(height/4))).^2);
     
     height = size(image,1);
     width = size(image,2);
-    crop_width = 900;
-    crop_height = 900;
+    crop_width = width*.25;
+    crop_height = height*.5;
     
     %%% Crops Width
     % Creates a scalar function which is used to scale summed pixel value
@@ -17,24 +17,9 @@ function cropped_image = AutoCropper(image)
     values = sum((image(:,:,2)));
     values = values.*scalar;
 
-    % finds a number of local maxima across the distribution and what their
-    % indicies are
-    TF = islocalmax(values,'MinSeparation',width/8);
-    indicies = find(TF == 1);
-
-    % determines the index closest to the middle point of the image
-    [~,idx] = min(abs(indicies - width/2));
-
-    % determines the 2nd closest index to middle point
-    indicies2 = indicies(indicies~=indicies(idx));
-    [~,idx2] = min(abs(indicies2 - width/2));
-
-    % determines which of the two points has more green in a given column
-    if values(indicies(idx)) > values(indicies2(idx2))
-        centerpoint = indicies(idx);
-    else
-        centerpoint = indicies2(idx2);
-    end
+    % finds the max of the scaled function
+    m = max(values);
+    centerpoint = find(values == m);
 
     % crops the iamge to a preset width around the center point
     cropped_image = imcrop(image,[centerpoint-crop_width/2,0,crop_width,height]);
@@ -54,5 +39,5 @@ function cropped_image = AutoCropper(image)
     
     % crops the image to a pre set height based upon the max
 %     cropped_image = imcrop(cropped_image,[0,index+floor(height/48),width,crop_height]);
-    cropped_image = imcrop(cropped_image,[0,index-96,width,crop_height]);
+    cropped_image = imcrop(cropped_image,[0,index+128,width,crop_height]);
 end
