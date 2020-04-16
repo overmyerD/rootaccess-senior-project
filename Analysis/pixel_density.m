1233ad3b67fa927022457d5296b24ee33d651384
Dalton Overmyer <dalton@raptorpowersystems.com>
2020-04-16 15:41:20 -0500

    bug fixed where snf extra entries were in speed and direction

--- Analysis/pixel_density.m
+++ Analysis/pixel_density.m
@@ -21,24 +21,24 @@ height = size(reduced_pix_density_xyz, 2);
 direction = zeros(1, width*height);
 speed = zeros(1, width*height);
 
-% for i = 1:width
-%     for j = 1:height
-%         x = i - orgin_x;
-%         y = j - orgin_y;
-%         theta = atan2(y, x) * 180 / pi;
-%         if theta < 0 
-%             theta = theta + 360;
-%         end
-%         direction(i * height + j) = theta;
-%         speed(i * height + j) = reduced_pix_density_xyz(i, j);
-%     end
-% end
-% 
+for i = 1:width
+    for j = 1:height
+        x = i - orgin_x;
+        y = j - orgin_y;
+        theta = atan2(y, x) * 180 / pi;
+        if theta < 0 
+            theta = theta + 360;
+        end
+        direction(i * height + j) = theta;
+        speed(i * height + j) = reduced_pix_density_xyz(i, j);
+    end
+end
+
 bar3(reduced_pix_density_xyz);
-% Options.AngleNorth     = 0;
-% Options.AngleEast      = 90;
-% Options.FreqLabelAngle = 10;
-% Options.ndirections = 25;
-% WindRose(direction, speed,Options);
+Options.AngleNorth     = 0;
+Options.AngleEast      = 90;
+Options.FreqLabelAngle = 10;
+Options.ndirections = 25;
+WindRose(direction, speed,Options);
 
 
