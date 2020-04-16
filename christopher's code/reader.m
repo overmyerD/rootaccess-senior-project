0d7c835bc4a95c59fc1c083b4a1ca04765b7ddef
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-16 15:33:41 -0500

    assorted changes

--- christopher's code/reader.m
+++ christopher's code/reader.m
@@ -1,17 +1,18 @@
-rootdir = './senior_project/040120 testing';
-outputdir = './senior_project/Output_test2/';
+rootdir = './senior_project/tstr';
+outputdir = './senior_project/output/';
+analysisdir = './senior_project/Analysis_test/';
 
 filelist = dir(fullfile(rootdir, '**/*.JPG'));  %get list of files and folders in any subfolder
 filelist = filelist(~[filelist.isdir]);  %remove folders from list
 
-% reads in all images in a given directory
-% ds = imageDatastore(filelist.folder);
-
 % loops through every image in the directory, skipping brightfield images
 for i = 1:size(filelist,1)
     if contains(filelist(i).name, "BF") == 0 && contains(filelist(i).name, "white") == 0
+        % get info about the filename and reads in image
+        [filepath,name,ext] = fileparts(filelist(i).name);
         image_name = strcat(filelist(i).folder,"/",filelist(i).name);
         image = imread(image_name);
+        
         % crops the original image down to a new size
         image = AutoCropper(image);
         
@@ -19,9 +20,14 @@ for i = 1:size(filelist,1)
         image = canny_algorithm(image);
         
         % insert analysis heres
+% %         data = reduce_pixel_density(image);
+% %         h = plot_pixel_density(data,"title",9);
+% %         saveas(h,strcat(analysisdir,name,"_analysis"),'jpg');
         
         % writes the new image to a given directory
-        new_filename = strcat(outputdir,filelist(i).name,"_cropped",".JPG");
+        new_filename = strcat(outputdir,name,"_cropped",".JPG");
         imwrite(image, new_filename);
     end
 end
+
+clear
