57e7891fad84a829319f221546b0c70fd338e217
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-18 22:40:38 -0500

    moved duplicate named files

--- Analysis/get_max_xy.m
+++
@@ -1,6 +0,0 @@
-function [x, y] = get_max_xy(matrix)
-%GET_MAX_XY Summary of this function goes here
-    maximum = max(max(matrix));
-    [x,y]=find(matrix==maximum);
-end
-
--- Analysis/reduce_pixel_density_matrix.m
+++
@@ -1,9 +0,0 @@
-function [reduced_pix_density_matrix, reduced_pix_density_1, reduced_pix_density_2, magnitude] = reduce_pixel_density_matrix(binaryImage1, binaryImage2, reduction)
-%REDUCE_PIXEL_DENSITY_MATRIX 
-    [reduced_pix_density_1, magnitude] = reduce_pixel_density(binaryImage1, reduction);
-    reduced_pix_density_2 = reduce_pixel_density(binaryImage2, reduction);
-    
-    reduced_pix_density_matrix = reduced_pix_density_1.' * reduced_pix_density_2;
-end
-
-
