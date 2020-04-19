28d657107c9d5b4c535d3f84d6c085d854f2cc6b
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-19 17:50:20 -0500

    added code for testing purposes

--- christopher's code/reader.m
+++ christopher's code/reader.m
@@ -1,10 +1,13 @@
-rootdir = './senior_project/Run1/10DAP';
-outputdir = './senior_project/Output_test/';
-analysisdir = './senior_project/Analysis_test/';
+tic;
+rootdir = './senior_project/run2/040120_21DAP';
+outputdir = './senior_project/Output21/';
+analysisdir = './senior_project/Analysis21/';
 
 filelist = dir(fullfile(rootdir, '**/*.JPG'));  %get list of files and folders in any subfolder
 filelist = filelist(~[filelist.isdir]);  %remove folders from list
 indicies = find(contains({filelist.name}, 'white')==1);
+filelist(indicies) = [];
+indicies = find(contains({filelist.name}, 'BF')==1);
 filelist(indicies) = [];
 raw_data2 = 0;
 set(groot,'defaultFigureVisible','off')
@@ -20,8 +23,12 @@ for i = 1:size(filelist,1)
         % crops the original image down to a new size
         image = AutoCropper(image);
         
+        
         % uses canny edge detection on cropped images
-        image = canny_algorithm(image);
+        c_image = canny_algorithm(image);
+        
+% temp test code
+image = imfuse(image, c_image, 'montage');
         % writes the new image to a given directory
         new_filename = strcat(outputdir,name,"_cropped",".JPG");
 %         new_filename = strcat(outputdir,string(i),".JPG");
@@ -30,26 +37,27 @@ for i = 1:size(filelist,1)
         %%% analysis of the post canny images
         
         % gets the numerical data of the image
-        [raw_data1,raw_data2] = reduce_pixel_density(image,raw_data2);
-        % plots the figure and returns a handle to the variable
-        h = plot_2Dpixel_density(raw_data2,"title",9);
-        % saves the individual plot using the handle
-        saveas(h,strcat(analysisdir,name,"_2D_analysis"),'jpg');
-        
-        if mod(i,2) == 0 && mod(i,4) ~= 0
-            % returns 3d bar graph, and raw numerical data of 3d bar graph
-            [h, raw_3D_data] = plot_3Dpixel_density(raw_data1, raw_data2);
-            saveas(h,strcat(analysisdir,name,"_3D_analysis"),'jpg');
-            
-            % wind rose
-            h = windrose_analysis(raw_3D_data);
-            saveas(h,strcat(analysisdir,name,"_windrose"),'jpg');
-        end
+%         [raw_data1,raw_data2] = reduce_pixel_density(image,raw_data2);
+%         % plots the figure and returns a handle to the variable
+%         h = plot_2Dpixel_density(raw_data2,"title",9);
+%         % saves the individual plot using the handle
+%         saveas(h,strcat(analysisdir,name,"_2D_analysis"),'jpg');
+%         
+%         if mod(i,2) == 0 && mod(i,4) ~= 0
+%             % returns 3d bar graph, and raw numerical data of 3d bar graph
+%             [h, raw_3D_data] = plot_3Dpixel_density(raw_data1, raw_data2);
+%             saveas(h,strcat(analysisdir,name,"_3D_analysis"),'jpg');
+%             
+%             % wind rose
+%             h = windrose_analysis(raw_3D_data);
+%             saveas(h,strcat(analysisdir,name,"_windrose"),'jpg');
+%         end
     end
     
     if mod(i,4) == 0
         fprintf("%d out of %d completed\n",i,size(filelist,1))
     end
 end
-
+toc
 clear;
+% delete(findall(0));
