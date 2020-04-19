e106dd80bace2fbc25639c7a87e19c2c95307380
Christopher McNeil <christophermcneil9@outlook.com>
2020-04-19 17:49:48 -0500

    added additional filters

--- Thomas's Code/canny_algorithm.m
+++ Thomas's Code/canny_algorithm.m
@@ -1,27 +1,44 @@
-function canny_image = canny_algorithm(image)
+function image = canny_algorithm(image)
     % converts image from color to gray scale
-    image = rgb2gray(image);
+%     image = rgb2gray(image);
+    image = image(:,:,2);
+    image = imadjust(image);
+    image = imsharpen(image);
+
+    
+    image = medfilt2(image);
+    
+%     PSF = fspecial('gaussian');
+%     image = deconvlucy(image,PSF);
+%     [image,~] = deconvblind(image,PSF);
+%     image(image<64) = 0;
+    
+%     contrast limited adaptive histogram equalization (CLAHE)
+    image = adapthisteq(image);
     
     % covert image to double (gaussian filter requires double)
-    image = double(image);
+%     image = double(image);
     
-    % gaussian filter to remove noise
+    % filter
     sigma = 6;
     masksize = 2*(sigma*2.5);
     filter = fspecial('gaussian', masksize, sigma);
-    
-    % convolution with gaussian filter
-    image = conv2(image, filter, 'valid');
 
-    % generate threshold values using mean and stdv of image     
+%     sigma = std2(image);
+%     filter = imgaussfilt(sigma);
+%     image = imgaussfilt(image);
+%     
+%     % convolution with gaussian filter
+    image = conv2(image, filter, 'valid');
+%     image = imfilter(image,filter);
+%     image = adapthisteq(image);
+%     
+% % % % %     % generate threshold values using mean and stdv of image  
     lowthresh = max(1,(mean2(image)-std2(image)))/255;
     highthresh = min(254,(mean2(image)+std2(image)))/255;
     thresh = [lowthresh highthresh];
-    
-%     disp(thresh);
-    
-    canny_image = edge(image,'canny',thresh);
-    
-    
+% % % % %     %     disp(thresh);
+% % % % % 
+    image = edge(image,'canny',thresh);
 end
 
